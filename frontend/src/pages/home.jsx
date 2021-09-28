865797a887af1e64d69a25bbea2762984ce04a1f
Piyush Jain <piyushmbm45@gmail.com>
2021-09-28 19:55:54 +0530

    Making home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from "react";
 
-function Home(){
-    return <h1>Home Page</h1>
+import ProductsComp from "../components/ProductsComp";
+
+function Home() {
+  return (
+    <>
+      <div className="hero py-16">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="w-1/2">
+            <h6 className="text-lg">
+              <em>Are You Hungry?</em>
+            </h6>
+            <h1 className="text-3xl md:text-6xl font-bold">Dont Wait !</h1>
+            <button className="px-6 py-2 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600">
+              Order Now
+            </button>
+          </div>
+          <div className="w-1/2">
+            <img
+              className="w-11/12"
+              src="/images/front1.png"
+              alt="front-burger"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="pb-24">
+        <ProductsComp />
+      </div>
+    </>
+  );
 }
 
 export default Home;
