920f2be1dc2e6d3befa0f47339a6d39bb493dc50
Piyush Jain <piyushmbm45@gmail.com>
2021-09-28 17:07:19 +0530

    seeting up link and images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
+import Home from "./pages/home";
+import About from "./pages/About";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <h1>Pizzano</h1>
+    <>
+      <Router>
+        <Navigation/>
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+          <Route path="/about" component={About} exact></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
